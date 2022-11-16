8e7ca4b75f18de0fbb5104ba2a5a343435115fdf
cincolunes <nihs4115@gmail.com>
2022-11-16 20:58:27 +0900

    Set Basic Register Form

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,24 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./view/NotFound";
 import Main from "./view/Main";
 import Header from "./component/Header";
 import Example from "./view/Example";
+import Register from "./view/Register";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Header/>
-                <Routes>
-                    <Route path={"/"} element={<Main/>}></Route>
-                    <Route path={"/example/*"} element={<Example/>}></Route>
-                    <Route path={"*"} element={<NotFound/>}></Route>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path={"/"} element={<Main />}></Route>
+          <Route path={"/register"} element={<Register />}></Route>
+          <Route path={"/example/*"} element={<Example />}></Route>
+          <Route path={"*"} element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- front/src/component/Header.js
+++ front/src/component/Header.js
@@ -1,13 +1,16 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Header = (props) =>{
-    return(
-        <>
-            <Link to ="/">
-                <h1>main으로 돌아가기</h1>
-            </Link>
-        </>
-    )
-}
+const Header = (props) => {
+  return (
+    <>
+      <Link to="/">
+        <h1>main으로 돌아가기</h1>
+      </Link>
+      <Link to="/register">
+        <h1>REGISTER</h1>
+      </Link>
+    </>
+  );
+};
 
 export default Header;
