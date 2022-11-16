0196469cf41978f1924d92c6013f82ca6abaa3bb
situodys <situodys@gmail.com>
2022-11-17 02:02:55 +0900

    refactor(RestaurantService): 인터페이스 삭제

--- back/src/main/java/kw/ic/backend/domain/restaurant/service/RestaurantService.java
+++ back/src/main/java/kw/ic/backend/domain/restaurant/service/RestaurantService.java
@@ -1,18 +1,46 @@
 package kw.ic.backend.domain.restaurant.service;
 
+import javax.persistence.EntityNotFoundException;
+import kw.ic.backend.domain.restaurant.dto.RestaurantResponseAssembler;
 import kw.ic.backend.domain.restaurant.dto.request.RestaurantPageRequest;
 import kw.ic.backend.domain.restaurant.dto.request.RestaurantRequest;
 import kw.ic.backend.domain.restaurant.dto.response.RestaurantPageResponse;
 import kw.ic.backend.domain.restaurant.dto.response.RestaurantResponse;
+import kw.ic.backend.domain.restaurant.entity.Restaurant;
+import kw.ic.backend.domain.restaurant.repository.RestaurantRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface RestaurantService {
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class RestaurantService {
 
-    RestaurantPageResponse findRestaurants(RestaurantPageRequest request);
+    private final RestaurantRepository restaurantRepository;
+    private final RestaurantResponseAssembler responseAssembler;
 
-    RestaurantResponse findById(Long id);
+    public RestaurantPageResponse findRestaurants(RestaurantPageRequest request) {
+        return null;
+    }
 
-    Long register(RestaurantRequest request);
+    public RestaurantResponse findById(Long id) {
+        Restaurant restaurant = restaurantRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
 
-    void delete(Long restaurant_id);
+        return responseAssembler.restaurantResponse(restaurant);
+    }
 
+    public Long register(RestaurantRequest request) {
+        Restaurant restaurant = restaurantRepository.save(request.toRestaurant());
+        return restaurant.getId();
+    }
+
+    public void delete(Long restaurant_id) {
+        log.info("delete restaurant_Id: {}", restaurant_id);
+
+        restaurantRepository.deleteById(restaurant_id);
+    }
 }
