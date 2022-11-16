8756671d7f5f5101748a8cfa0ae3a18ac109c519
situodys <situodys@gmail.com>
2022-11-16 12:30:25 +0900

    feat(Restaurant): repository 테스트 설정"

---
+++ back/src/test/java/kw/ic/backend/restaurant/repository/RestaurantRepositoryTest.java
@@ -0,0 +1,73 @@
+package kw.ic.backend.restaurant.repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.stream.IntStream;
+import kw.ic.backend.domain.restaurant.dto.request.RestaurantRequest;
+import kw.ic.backend.domain.restaurant.entity.Restaurant;
+import kw.ic.backend.domain.restaurant.dto.embbed.Address;
+import kw.ic.backend.domain.restaurant.dto.embbed.RestaurantType;
+import kw.ic.backend.domain.restaurant.dto.embbed.RunningTime;
+import kw.ic.backend.domain.restaurant.repository.RestaurantRepository;
+import kw.ic.backend.global.config.JpaConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@Import(JpaConfig.class)
+@TestInstance(Lifecycle.PER_CLASS)
+public class RestaurantRepositoryTest {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    @BeforeAll
+    private void init() {
+        IntStream.rangeClosed(1,5)
+                .forEach(idx ->{
+                    RestaurantRequest request = new RestaurantRequest(RestaurantType.KOREAN,
+                            new Address("city"+idx, "street"+idx, "zipcode"+idx),
+                            new RunningTime(LocalDateTime.now(), LocalDateTime.now()));
+
+                    restaurantRepository.save(request.toRestaurant());
+                });
+    }
+
+    @Test
+    @DisplayName("식당 정상 등록 확인")
+    public void register() throws Exception {
+        //given
+        RestaurantRequest request = new RestaurantRequest(RestaurantType.KOREAN,
+                new Address("city1", "street1", "zipcode"),
+                new RunningTime(LocalDateTime.now(), LocalDateTime.now())
+        );
+
+        //when
+        Restaurant restaurant = restaurantRepository.save(request.toRestaurant());
+
+        //then
+        Assertions.assertThat(restaurant.getId()).isEqualTo(6L);
+    }
+
+    @Test
+    @DisplayName("restaurant_id로 restaurant 찾기")
+    public void findById() throws Exception {
+        //given
+        Long id = 1L;
+
+        //when
+        Optional<Restaurant> restaurant = restaurantRepository.findById(id);
+
+        //then
+        Assertions.assertThat(restaurant.isPresent()).isEqualTo(true);
+    }
+}
