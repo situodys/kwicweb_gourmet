5b95f834294ba3180924a0d1357a6c798eb282bc
situodys <situodys@gmail.com>
2022-11-21 22:02:31 +0900

    feat(Review): controller 구현

---
+++ back/src/main/java/kw/ic/backend/domain/review/controller/ReviewController.java
@@ -0,0 +1,63 @@
+package kw.ic.backend.domain.review.controller;
+
+import kw.ic.backend.domain.review.dto.request.ReviewPageRequest;
+import kw.ic.backend.domain.review.dto.request.ReviewRequest;
+import kw.ic.backend.domain.review.dto.response.ReviewPageResponse;
+import kw.ic.backend.domain.review.dto.response.ReviewResponse;
+import kw.ic.backend.domain.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/reviews")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @GetMapping("")
+    ResponseEntity<ReviewPageResponse> findReviews(@RequestBody @Validated ReviewPageRequest request) {
+        log.info("find reviews of restaurantId: {} with pagination", request);
+
+        ReviewPageResponse response = reviewService.findReviews(request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{review_id")
+    ResponseEntity<ReviewResponse> findReview(@PathVariable Long reviewId) {
+        log.info("find review by id: {}", reviewId);
+
+        ReviewResponse response = reviewService.findReview(reviewId);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/")
+    ResponseEntity<Long> register(@RequestBody ReviewRequest request) {
+        log.info("register review of restaurantId: {}", request);
+
+        Long reviewId = reviewService.register(request);
+
+        return ResponseEntity.ok(reviewId);
+    }
+
+    @DeleteMapping("/{review_id")
+    ResponseEntity<Long> delete(@PathVariable(name = "review_id") Long reviewId) {
+        log.info("delete reviewId: {}",reviewId);
+
+        Long deletedId = reviewService.deleteById(reviewId);
+
+        return ResponseEntity.ok(deletedId);
+    }
+}
