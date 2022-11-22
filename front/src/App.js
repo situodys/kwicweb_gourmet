7bf41137dbf3aac499f1d5003e054c9e2c9104ca
cincolunes <nihs4115@gmail.com>
2022-11-22 21:09:21 +0900

    feat(Navigation Bar): 기본 Navigation Bar 구현 - cincolunes

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./view/NotFound";
 import Main from "./view/Main";
+
+import NavBar from "./component/NavBar";
 import Header from "./component/Header";
 import Example from "./view/Example";
 import Login from "./view/Login";
 import Register from "./view/Register";
 
-
 function App() {
   return (
     <div className="App" style={{ height: "100vh" }}>
       <BrowserRouter>
-        {/* <Header /> */}
+        <NavBar />
+        <Header />
         <Routes>
           <Route path={"/"} element={<Main />} exact={true}></Route>
           <Route path={"/login"} element={<Login />}></Route>
