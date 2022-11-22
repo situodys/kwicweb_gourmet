60b63e068ef39770857c36c3a098b062f3b80e25
situodys <situodys@gmail.com>
2022-11-22 14:50:45 +0900

    feat(Proposal): controller 구현

---
+++ back/src/main/java/kw/ic/backend/domain/proposal/controller/ProposalController.java
@@ -0,0 +1,52 @@
+package kw.ic.backend.domain.proposal.controller;
+
+import kw.ic.backend.domain.proposal.dto.request.ProposalPageRequest;
+import kw.ic.backend.domain.proposal.dto.request.ProposalRequest;
+import kw.ic.backend.domain.proposal.dto.response.ProposalPageResponse;
+import kw.ic.backend.domain.proposal.service.ProposalService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/proposals")
+public class ProposalController {
+
+    private final ProposalService proposalService;
+
+    @GetMapping("")
+    public ResponseEntity<ProposalPageResponse> findProposals(@RequestBody ProposalPageRequest request) {
+        log.info(" findProposals: {}", request);
+
+        ProposalPageResponse response = proposalService.findProposals(request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Long> register(@RequestBody ProposalRequest proposalRequest) {
+        log.info("register proposal");
+
+        Long proposalId = proposalService.register(proposalRequest);
+
+        return ResponseEntity.ok(proposalId);
+    }
+
+    @DeleteMapping("/{proposal_id}")
+    public ResponseEntity<Long> delete(@PathVariable(name="proposal_id") Long proposalId) {
+        log.info("delete proposal of Id: {}", proposalId);
+
+        Long deleteId = proposalService.delete(proposalId);
+
+        return ResponseEntity.ok(deleteId);
+    }
+}
