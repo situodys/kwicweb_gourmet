845ebe81ffdcb557378b1981688f5ba1de264862
situodys <situodys@gmail.com>
2022-11-22 10:26:57 +0900

    refactor(ReviewedMenu): registerAll 메서드 매개 변수 수정

--- back/src/main/java/kw/ic/backend/domain/menu/service/ReviewedMenuService.java
+++ back/src/main/java/kw/ic/backend/domain/menu/service/ReviewedMenuService.java
@@ -6,6 +6,7 @@ import kw.ic.backend.domain.menu.ReviewedMenu;
 import kw.ic.backend.domain.menu.dto.SimpleMenu;
 import kw.ic.backend.domain.menu.repository.MenuRepository;
 import kw.ic.backend.domain.menu.repository.ReviewedMenuRepository;
+import kw.ic.backend.domain.review.entity.Review;
 import kw.ic.backend.domain.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,14 +20,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReviewedMenuService {
 
     private final ReviewedMenuRepository reviewedMenuRepository;
-    private final ReviewRepository reviewRepository;
     private final MenuRepository menuRepository;
 
-     public int registerAll(List<SimpleMenu> simpleMenus, Long reviewId) {
+     public int registerAll(List<SimpleMenu> simpleMenus, Review review) {
         List<ReviewedMenu> reviewedMenus = simpleMenus.stream()
                 .map(simpleMenu -> ReviewedMenu.builder()
                         .menuName(simpleMenu.getMenuName())
-                        .review(reviewRepository.getReferenceById(reviewId))
+                        .review(review)
                         .menu(menuRepository.getReferenceById(simpleMenu.getMenuId()))
                         .build())
                 .collect(Collectors.toUnmodifiableList());
