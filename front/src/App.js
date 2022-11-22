84bc4e06610e5722c7b16a10905910a9179f18f5
situodys <situodys@gmail.com>
2022-11-23 02:20:31 +0900

    chore: 오탈자 및 패키지 경로 수정

--- front/src/App.js
+++ front/src/App.js
@@ -2,11 +2,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./view/NotFound";
 import Main from "./view/Main";
 
-import NavBar from "./component/NavBar";
-import Login from "./component/Login";
-import Register from "./component/Register";
+import NavBar from "./component/global/NavBar";
+import Login from "./view/auth/Login";
+import Register from "./view/auth/Register";
 
-import Resturants from "./component/Resturants";
+import Restaurants from "./view/Restaurants";
 import Example from "./view/Example";
 
 function App() {
@@ -20,7 +20,7 @@ function App() {
 
           <Route path={"/register"} element={<Register />}></Route>
           <Route path={"/example/*"} element={<Example />}></Route>
-          <Route path={"/resturants"} element={<Resturants />}></Route>
+          <Route path={"/restaurants"} element={<Restaurants />}></Route>
           <Route path={"*"} element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
