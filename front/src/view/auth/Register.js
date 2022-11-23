c654f7c8b3c95fce2a62d8f3d1f6693de45c36b5
situodys <situodys@gmail.com>
2022-11-23 11:31:44 +0900

    feat(Register): 회원가입 기능 구현

--- front/src/view/auth/Register.js
+++ front/src/view/auth/Register.js
@@ -1,8 +1,135 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../../component/auth/css/register.css";
 import CustomInput from "../../component/auth/CustomInput";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 const Register = (props) => {
+
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [authCode, setAuthCode] = useState("");
+
+    const [isAuthenticatedEmail, setIsAuthenticatedEmail] = useState(false);
+    const [isDuplicatedEmail, setIsDuplicatedEmail] = useState(false);
+    const [isValidPassword, setIsValidPassword] = useState(false);
+    const [isValidPasswordConfirm, setIsValidPasswordConfirm] = useState(false);
+    const [isLogin, setIsLogin] = useState(false);
+    const [isEmailSent, setIsEmailSent] = useState(false);
+
+    const [passwordConfirmMsg, setPasswordConfirmMsg] = useState("");
+
+    const [duplicateIdMessage, setDuplicateIdMessage] = useState("");
+
+    const checkLogin = () => {
+        const tk = window.sessionStorage.getItem("jwt");
+        if (tk) {
+            navigate("/home");
+        }
+        setIsLogin(true);
+    }
+
+    useEffect(() => {
+        if (!isLogin) {
+            checkLogin();
+        }
+        checkConfirm();
+    }, [password, passwordConfirm])
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        setDuplicateIdMessage("");
+        setIsAuthenticatedEmail(false);
+        setIsDuplicatedEmail(false);
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+        if (password.length !== 0) {
+            setIsValidPassword(true);
+        } else {
+            setIsValidPassword(false);
+        }
+        setIsValidPasswordConfirm(false);
+    }
+    const handlePasswordConfirm = (e) => {
+        e.preventDefault();
+        setPasswordConfirm(e.target.value);
+    }
+
+    const handleEmailSend = async (e) => {
+        try {
+            if (await isAlreadySignup(e)) {
+                return;
+            }
+            await sendEmail();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const isAlreadySignup = async () => {
+        const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/member/${email}`);
+        if (response.data && response.data == true) {
+            setDuplicateIdMessage("등록된 이메일입니다.");
+            setIsDuplicatedEmail(true);
+            setIsAuthenticatedEmail(false);
+            return true;
+        }
+        return false;
+    }
+
+    const sendEmail = async () => {
+        window.alert("이메일을 확인해주세요!")
+        setIsEmailSent(true);
+        const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/email/auth/${email}`);
+    }
+
+    const handleAuthCode = (e) => {
+        e.preventDefault();
+        setAuthCode(e.target.value);
+        setIsAuthenticatedEmail(false);
+    }
+
+    const handleEmailAuthSubmit = async () => {
+        const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/email/auth/${email}/code`);
+        if (response.data && response.data === authCode) {
+            setIsAuthenticatedEmail(true);
+            return;
+        }
+    }
+
+    const handleRegisterSubmit = async () => {
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/auth/signup`, toData());
+            if (response.status == 200) {
+                window.alert("정상적으로 가입되었습니다");
+                navigate("/login");
+            }
+        } catch (err) {
+            console.log("err");
+        }
+    }
+
+    const checkConfirm = () => {
+        if (!passwordConfirm) {
+            setPasswordConfirmMsg("");
+        } else if (passwordConfirm && passwordConfirm === password) {
+            setPasswordConfirmMsg("비밀번호가 일치합니다.");
+            setIsValidPasswordConfirm(true);
+        } else {
+            setPasswordConfirmMsg("비밀번호가 다릅니다. 다시 확인해주세요");
+            setIsValidPasswordConfirm(false);
+        }
+    }
+
+    const toData = () => {
+        const data = {};
+        data['email'] = email;
+        data['password'] = Buffer.from(password).toString('base64');
+        return data;
+    }
 
     return (
         <>
@@ -22,20 +149,46 @@ const Register = (props) => {
                                         </p>
 
                                         <CustomInput type={"email"}
-                                                     placeHolder={"Email"}></CustomInput>
+                                                     placeHolder={"Email"}
+                                                     handleInput={handleEmail}></CustomInput>
                                         <button
-                                            className="btn btn-outline-light btn-lg col-12 mb-4"
+                                            className="btn btn-outline-light btn-lg col-8 mb-4"
                                             type="submit"
+                                            onClick={handleEmailSend}
                                         >
                                             E-mail Authentication
                                         </button>
+
+                                        {isEmailSent && <CustomInput type={"text"}
+                                                                     placeHolder={"auth code"}
+                                                                     handleInput={handleAuthCode}></CustomInput>}
+
+                                        {isEmailSent && <button
+                                            className="btn btn-outline-light btn-lg col-4 mb-4"
+                                            type="submit"
+                                            onSubmit={handleEmailAuthSubmit}
+                                        >
+                                            check
+                                        </button>}
+
                                         <CustomInput type={"password"}
-                                                     placeHolder={"Password"}></CustomInput>
+                                                     placeHolder={"Password"}
+                                                     handleInput={handlePassword}></CustomInput>
                                         <CustomInput type={"password"}
-                                                     placeHolder={"Password"}></CustomInput>
+                                                     placeHolder={"PasswordConfirm"}
+                                                     handleInput={handlePasswordConfirm}></CustomInput>
+                                        <div
+                                            className={" mb-5"}
+                                            style={{
+                                            color: isValidPasswordConfirm ? 'green' : 'red',
+                                            height: "20px"
+                                        }}>{passwordConfirmMsg}</div>
+
                                         <button
+                                            disabled={!(isAuthenticatedEmail && !isDuplicatedEmail && isValidPasswordConfirm && isValidPassword)}
                                             className="btn btn-outline-light btn-lg px-5"
                                             type="submit"
+                                            onClick={handleRegisterSubmit}
                                         >
                                             Register
                                         </button>
@@ -44,7 +197,7 @@ const Register = (props) => {
                                     <div>
                                         <p className="mb-0">
                                             Have already an account?{" "}
-                                            <a href="src/view/auth/Register#!" className="text-white-50 fw-bold">
+                                            <a href="/login" className="text-white-50 fw-bold">
                                                 Login here
                                             </a>
                                         </p>
