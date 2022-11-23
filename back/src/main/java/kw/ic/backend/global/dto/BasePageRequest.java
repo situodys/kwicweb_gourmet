e47501453264fbf78f4145fcb97418e2f47fa224
situodys <situodys@gmail.com>
2022-11-24 01:00:53 +0900

    feat(Global): BasePageRequest DTO 추가

---
+++ back/src/main/java/kw/ic/backend/global/dto/BasePageRequest.java
@@ -0,0 +1,45 @@
+package kw.ic.backend.global.dto;
+
+import java.util.List;
+import javax.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.PageRequest;
+
+@Setter
+@AllArgsConstructor
+public class BasePageRequest {
+
+    private static final long NEED_CALCULATE = -1L;
+
+    @Min(value = 0L)
+    private Integer page;
+
+    @Min(value = 1L)
+    private Integer size;
+
+    private List<String> sorts;
+
+    private Long totalCount;
+
+    public PageRequest getPageRequest() {
+        return PageRequest.of(getPage(), getSize());
+    }
+
+    public Integer getPage() {
+        return page == null ? 0 : page;
+    }
+
+    public Integer getSize() {
+        return size == null ? 1 : size;
+    }
+
+    public List<String> getSorts() {
+        return this.sorts;
+    }
+
+    public Long getTotalCount() {
+        return totalCount == null ? NEED_CALCULATE : totalCount;
+    }
+}
