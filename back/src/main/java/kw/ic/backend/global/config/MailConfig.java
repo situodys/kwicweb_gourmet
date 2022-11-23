54e4b2954b1e9a34c7ef1ceefc7faccf35e97089
situodys <situodys@gmail.com>
2022-11-23 13:31:38 +0900

    feat(Mail): Async 설정 추가

--- back/src/main/java/kw/ic/backend/global/config/MailConfig.java
+++ back/src/main/java/kw/ic/backend/global/config/MailConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
+@EnableAsync
 public class MailConfig {
 
     @Value("${mail.smtp.port}")
