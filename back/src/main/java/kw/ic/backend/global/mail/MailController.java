8d648c0f928406e70cc8cc90dc96b140ce49a223
situodys <situodys@gmail.com>
2022-11-29 12:27:53 +0900

    feat(BE): 이메일 인증 및 전송 수정

--- back/src/main/java/kw/ic/backend/global/mail/MailController.java
+++ back/src/main/java/kw/ic/backend/global/mail/MailController.java
@@ -1,26 +1,28 @@
 package kw.ic.backend.global.mail;
 
-import java.util.Base64;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/email")
+@RequestMapping("/api/auth")
 public class MailController {
 
     private final MailService mailService;
-    private static final Base64.Decoder decoder = Base64.getDecoder();
 
-    @GetMapping("/auth/{email}")
-    public ResponseEntity<String> sendAuthEmail(@PathVariable("email") String emailAddress) throws Exception{
-//        String decodedAddress = String.valueOf(decoder.decode(emailAddress));
-        String authCode = mailService.sendAuthCodeEmail(emailAddress);
+    @PostMapping("/email")
+    public ResponseEntity<String> sendAuthEmail(@RequestBody MailRequest mailRequest) throws Exception{
+        String authCode = mailService.sendAuthCodeEmail(mailRequest.getEmail());
 
         return ResponseEntity.ok(authCode);
+    }
+
+    @PostMapping("/email/health")
+    public ResponseEntity<Boolean> isAlreadySignupEmail(@RequestBody MailRequest mailRequest) throws Exception {
+        return ResponseEntity.ok(mailService.isAlreadySignupEmail(mailRequest.getEmail()));
     }
 }
