25e5d952c9246cb5d7c271b48ce35607093deb12
cincolunes <nihs4115@gmail.com>
2022-12-02 02:07:49 +0900

    Feat(Proposal): Proposal 기본 틀 구현

---
+++ front/src/component/ProposalModal.js
@@ -0,0 +1,72 @@
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Stack from "react-bootstrap/Stack";
+import { useState } from "react";
+
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+
+const ProposalModal = ({ show, handleClose }) => {
+  const [menuList, setMenuList] = useState(["temp", "temp2"]);
+  const [selectedMenu, setSelectedMenu] = useState("f");
+
+  const changeStatus = (event, eventkey) => {
+    setSelectedMenu(eventkey.target.innerText);
+  };
+
+  return (
+    <>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header className=" border-0" closeButton></Modal.Header>
+        <Modal.Title className="text-center w-100">
+          <strong>Write a Proposal :)</strong>
+        </Modal.Title>
+
+        <Modal.Body>
+          <div className="d-flex justify-content-center">
+            <div className="mb-5"></div>
+          </div>
+          <Form>
+            <Stack gap={3}>
+              <DropdownButton
+                id="dropdown-menu-button"
+                title="Menu"
+                // onSelect={(e) => {
+                //   console.log("F", e);
+                //   setSelectedMenu(e);
+                // }}
+                onSelect={changeStatus}
+              >
+                {menuList.map((menu) => (
+                  <Dropdown.Item key={menu}>{menu}</Dropdown.Item>
+                ))}
+              </DropdownButton>
+
+              <Form.Control type="text" placeholder="Review title" />
+
+              <Form.Control as="textarea" rows={12} />
+              <div className="d-flex justify-content-center">
+                <Button className="col-6" onClick={handleClose}>
+                  Submit
+                </Button>
+              </div>
+            </Stack>
+          </Form>
+        </Modal.Body>
+
+        <hr className="m-0" />
+        <Modal.Footer className="border-0"></Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+export default ProposalModal;
