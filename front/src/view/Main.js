b91ff1e0462c691e1681102be710b2638853a48f
cincolunes <nihs4115@gmail.com>
2022-12-02 00:54:19 +0900

    Feat & Refactor & Chore (Main): 메인 화면 구현 (추가 작업 필요)

--- front/src/view/Main.js
+++ front/src/view/Main.js
@@ -1,14 +1,65 @@
-import { RestaurantList } from "../component/restaurant/RestaurantList";
+import { RestaurantList } from "../component/main/RestaurantList";
+import { RestaurantRecommendation } from "../component/main/RestaurantRecommendation";
 import { Map } from "../component/main/Map";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import customAxios from "../api/customAxios";
+import Stack from "react-bootstrap/Stack";
 
 export default function Main(props) {
+  const [topLikes, setTopLikes] = useState([]);
+  const [topRatings, setTopRatings] = useState([]);
+
+  const handleTopList = async () => {
+    try {
+      const responseLikesTop5 = await customAxios.get("/restaurants/likes/top");
+      const responseRatesTop5 = await customAxios.get(
+        "/restaurants/reviews/rating/top"
+      );
+
+      if (
+        responseLikesTop5.status === 200 &&
+        responseRatesTop5.status === 200
+      ) {
+        setTopLikes(responseLikesTop5.data);
+        setTopRatings(responseRatesTop5.data);
+      }
+    } catch (err) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  useEffect(() => {
+    handleTopList();
+  }, []);
+
   return (
     <>
-      <div className="container-fluid Login-body">
-        <div className="row p-5">
-          <div className="col-4">
-            <div className="input-group mb-3 shadow-lg">
+      <div
+        className="d-flex Login-body justify-content-center"
+        style={{ height: "93vh", overflow: "hidden" }}
+      >
+        <div
+          className="row h-100"
+          style={{ maxWidth: "90rem", padding: "4vh" }}
+        >
+          <div className="col-4 h-100">
+            <div className="container-fluid h-100">
+              <div className="row">
+                <Map />
+              </div>
+              <div className="row">
+                <RestaurantRecommendation
+                  topLikes={topLikes}
+                  topRatings={topRatings}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="col-8 h-100">
+            <div className="input-group mb-3 cshadow-lg">
               <input type="search" class="form-control" placeholder=""></input>
               <button
                 class="btn btn btn-primary"
@@ -18,11 +69,6 @@ export default function Main(props) {
                 Search
               </button>
             </div>
-
-            <Map />
-          </div>
-
-          <div className="col-8">
             <RestaurantList />
           </div>
         </div>
