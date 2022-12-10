02c9ae786581135f3ee380501e9e5bd52111669c
situodys <situodys@gmail.com>
2022-12-10 18:17:30 +0900

    refactor: carousel 기능 삭제

--- front/src/component/restaurant/core/RestaurantCarousel.js
+++ front/src/component/restaurant/core/RestaurantCarousel.js
@@ -21,30 +21,6 @@ const RestaurantCarousel = (props) => {
               </div>
             </div>
           </div>
-          <a
-            className="carousel-control-prev"
-            href="src/component/restaurant/core/RestaurantCarousel#carouselExampleControls"
-            role="button"
-            data-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a
-            className="carousel-control-next"
-            href="src/component/restaurant/core/RestaurantCarousel#carouselExampleControls"
-            role="button"
-            data-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="sr-only">Next</span>
-          </a>
         </div>
       </div>
     </>
